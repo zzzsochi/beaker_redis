382b80b74cc5172a14333df4ade777ad545d38fa
Alexander Zelenyak <zzz.sochi@gmail.com>
2016-07-19 10:53:05 +0300

    Add TTL for sessions and refactoring the code

--- beaker_redis.py
+++ beaker_redis.py
@@ -1,8 +1,8 @@
-""" Beaker backend for redis
+""" Beaker backend for redis.
 """
-
-import pickle
 import logging
+from pickle import dumps as pickle_dumps
+from pickle import loads as pickle_loads
 
 from redis import Redis
 from beaker.container import NamespaceManager, Container
@@ -10,22 +10,25 @@ from beaker.util import verify_directory
 
 log = logging.getLogger(__name__)
 
+DEFAULT_TTL = 30 * 24 * 60 * 60  # 30 days
+
 
 class RedisBackend(NamespaceManager):
-    """ Beaker backend for redis
+    """ Beaker backend for redis.
 
-    :param str hkey: name for redis hkey for store data
-        (default: `sessions`)
+    :param str hkey_prefix: name for redis hkey for store data
+        (default: `session`)
+    :param str ttl: ttl from last access
+        (default: `2592000` (30 days))
     """
-    hkey = 'sessions'
-
     def __init__(
             self,
             namespace,
-            url,
+            redis_uri='localhost:6379/0',
             data_dir=None,
             lock_dir=None,
-            hkey='sessions',
+            hkey_prefix='session',
+            ttl=DEFAULT_TTL,
             **params):
 
         super().__init__(namespace)
@@ -40,44 +43,46 @@ class RedisBackend(NamespaceManager):
         if self.lock_dir:
             verify_directory(self.lock_dir)
 
-        self.hkey = hkey
+        host, port_db = redis_uri.split(':', 1)
+        port, db_num = (int(i) for i in port_db.split('/', 1))
 
-        host, port_db = url.split(':', 1)
-        port, db = (int(i) for i in port_db.split('/', 1))
+        self.db = Redis(host=host, port=port, db=db_num)
 
-        self.db = Redis(host=host, port=port, db=db)
+        self.hkey = hkey = ':'.join((hkey_prefix, self.namespace))
+        self.ttl = int(ttl)
 
-        log.debug('sessions redis backend setuped: namespace={}'.format(namespace))
+        log.debug("session setuped: %s", hkey)
 
     def __contains__(self, key):
-        return self.db.hexists(self.hkey, self.format_key(key))
+        return self.db.hexists(self.hkey, key)
 
     def __getitem__(self, key):
-        data = self.db.hget(self.hkey, self.format_key(key))
+        data = self.db.hget(self.hkey, key)
 
         if data is not None:
-            return pickle.loads(data)
+            self.db.expire(self.hkey, self.ttl)
+            return pickle_loads(data)
         else:
             raise KeyError(key)
 
     def __setitem__(self, key, value):
-        key = self.format_key(key)
-        log.debug('session set key ({}: "{!r}")'.format(key, value))
-        self.db.hset(self.hkey, key, pickle.dumps(value))
+        log.debug("session set key: %s %s %r", self.hkey, key, value)
+        self.db.hset(self.hkey, key, pickle_dumps(value))
+        self.db.expire(self.hkey, self.ttl)
 
     def __delitem__(self, key):
-        key = self.format_key(key)
+        log.debug("session del key: %s %s", self.hkey, key)
         self.db.hdel(self.hkey, key)
-
-    def format_key(self, key):
-        return '{} {}'.format(self.namespace, key)
+        self.db.expire(self.hkey, self.ttl)
 
     def do_remove(self):
-        log.debug('remove sessions')
+        log.debug("session remove: %s", self.hkey)
         self.db.delete(self.hkey)
 
     def keys(self):
-        raise self.db.hkeys(self.hkey)
+        keys = self.db.hkeys(self.hkey)
+        self.db.expire(self.hkey, self.ttl)
+        return keys
 
 
 class RedisContainer(Container):
